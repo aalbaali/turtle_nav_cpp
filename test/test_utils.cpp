151595ec364f78a80fc102aa5659d02d454de59c
aalbaali <albaalia@live.com>
2022-03-02 21:41:36 -0500

    Add demo tests

---
+++ test/test_utils.cpp
@@ -0,0 +1,21 @@
+/**
+ * Copyright 2022 Ⓒ Amro Al-Baali
+ * @file test_utils.cpp
+ * @brief
+ * @author Amro Al-Baali (albaalia@live.com)
+ * @date 2022-Mar-02
+ */
+
+#include "gtest/gtest.h"
+#include "turtle_nav_cpp/utils.hpp"
+
+TEST(Demo, SomeTestDemo) {
+  EXPECT_TRUE(true);
+  EXPECT_TRUE(true);
+}
+
+TEST(Demo, SomeOtherTestDemo) {
+  EXPECT_TRUE(true);
+  EXPECT_TRUE(true);
+  EXPECT_TRUE(true);
+}
