8d5b07be39abb322aa1d4be41245f54f3a961c82
aalbaali <albaalia@live.com>
2022-03-06 13:00:15 +0000

    Add .pre-commit-config.yaml file (and hook)

--- .devcontainer/run_all.sh
+++ .devcontainer/run_all.sh
@@ -4,7 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source $SCRIPT_DIR/install_packages.sh
 
-source $SCRIPT_DIR/colcon_build.sh 
+source $SCRIPT_DIR/colcon_build.sh
 
 source $SCRIPT_DIR/colcon_test.sh
-
