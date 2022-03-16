7ec8c0d91bf3b11cfbefd8deab74841bc7b76ebd
aalbaali <albaalia@live.com>
2022-03-16 11:46:11 +0000

    Add empty skeleton of math_utils

---
+++ include/turtle_nav_cpp/math_utils.hpp
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2022 Ⓒ Amro Al-Baali
+ * @file math_utils.hpp
+ * @brief Math helper functions. This should be independent of ROS
+ * @author Amro Al-Baali (albaalia@live.com)
+ * @date 2022-Mar-16
+ */
+#ifndef TURTLE_NAV_CPP_MATH_UTILS_HPP_
+#define TURTLE_NAV_CPP_MATH_UTILS_HPP_
+
+#endif  // TURTLE_NAV_CPP_MATH_UTILS_HPP_
---
+++ src/math_utils.cpp
@@ -0,0 +1,7 @@
+/**
+ * Copyright 2022 Ⓒ Amro Al-Baali
+ * @file math_utils.cpp
+ * @brief Implementation of math_utils.hpp
+ * @author Amro Al-Baali (albaalia@live.com)
+ * @date 2022-Mar-16
+ */
