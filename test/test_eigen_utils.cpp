55004ab521d9d9f613dc8681e52f9be81064f92f
aalbaali <albaalia@live.com>
2022-03-19 14:23:20 +0000

    Add vector test

--- test/test_eigen_utils.cpp
+++ test/test_eigen_utils.cpp
@@ -39,7 +39,11 @@ TEST(StdVectorToMatrix, RowColMajor)
   }
 }
 
-TEST(MatrixToStdArr, RowMajorTwoByTwoMatrix)
+/**
+ * @brief Array shape should match the row major matrix data type
+ *
+ */
+TEST(MatrixToStdArray, RowMajorMatrix)
 {
   // Vector to initialize matrix (using the `Eigen::Map` function)
   std::vector<double> init_vals{1, 2, 3, 4, 5, 6};
@@ -63,7 +67,11 @@ TEST(MatrixToStdArr, RowMajorTwoByTwoMatrix)
   }
 }
 
-TEST(MatrixToStdArr, ColMajorTwoByTwoMatrix)
+/**
+ * @brief Array shape should match the column major matrix data type
+ *
+ */
+TEST(MatrixToStdArray, ColMajorMatrix)
 {
   // Vector to initialize matrix (using the `Eigen::Map` function)
   std::vector<double> init_vals{1, 2, 3, 4, 5, 6};
@@ -84,6 +92,39 @@ TEST(MatrixToStdArr, ColMajorTwoByTwoMatrix)
     for (int j = 0; j < cols; j++) {
       EXPECT_DOUBLE_EQ(arr_row_major[i + rows * j], mat(i, j));
     }
+  }
+}
+
+/**
+ * @brief Arrays from eigen vectors should be independent of their storage type
+ *
+ */
+TEST(MatrixToStdArray, EigVector)
+{
+  // Vector to initialize matrix (using the `Eigen::Map` function)
+  std::vector<double> init_vals{1, 2, 3, 4, 5, 6};
+
+  // Matrix size
+  const int sz = 6;
+
+  // Matrix data type. Note that it's row major in this test
+  using MatrixRowMajor = Eigen::Matrix<double, 1, sz, Eigen::StorageOptions::RowMajor>;
+  using MatrixColMajor = Eigen::Matrix<double, sz, 1, Eigen::StorageOptions::ColMajor>;
+
+  // Both matrices should be the same (i.e., when accessing coefficients)
+  // The difference is in the way the underlying data is stored
+  MatrixRowMajor mat_row_major = Eigen::Map<MatrixRowMajor>(init_vals.data());
+  MatrixColMajor mat_col_major = Eigen::Map<MatrixColMajor>(init_vals.data());
+
+  // Get the array
+  const auto arr_row_major = MatrixToStdArray(mat_row_major);
+  const auto arr_col_major = MatrixToStdArray(mat_col_major);
+
+  for (int i = 0; i < sz; i++) {
+    EXPECT_DOUBLE_EQ(arr_row_major[i], mat_row_major(1, i));
+    EXPECT_DOUBLE_EQ(arr_row_major[i], mat_col_major(i, 1));
+    EXPECT_DOUBLE_EQ(arr_col_major[i], mat_row_major(1, i));
+    EXPECT_DOUBLE_EQ(arr_col_major[i], mat_col_major(i, 1));
   }
 }
 
