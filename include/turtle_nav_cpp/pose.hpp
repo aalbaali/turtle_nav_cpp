6cb2c205dfc2496603cb32aa3bf44fb3cc5cfd8f
aalbaali <albaalia@live.com>
2022-03-21 01:41:10 +0000

    Add `pose` skeleton

---
+++ include/turtle_nav_cpp/pose.hpp
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2022 Ⓒ Amro Al-Baali
+ * @file pose.hpp
+ * @brief SE(2) pose class
+ * @author Amro Al-Baali (albaalia@live.com)
+ * @date 2022-Mar-21
+ */
+#ifndef TURTLE_NAV_CPP_POSE_HPP_
+#define TURTLE_NAV_CPP_POSE_HPP_
+
+#endif  // TURTLE_NAV_CPP_POSE_HPP_
---
+++ src/pose.cpp
@@ -0,0 +1,7 @@
+/**
+ * Copyright 2022 Ⓒ Amro Al-Baali
+ * @file pose.cpp
+ * @brief Implementation of pose.hpp
+ * @author Amro Al-Baali (albaalia@live.com)
+ * @date 2022-Mar-21
+ */
