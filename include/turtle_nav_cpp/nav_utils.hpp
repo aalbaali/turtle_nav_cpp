f627eeafc11cf8f2999438d0d0b75e9e705c7e88
aalbaali <albaalia@live.com>
2022-03-21 01:19:49 +0000

    Add nav_utils skeleton

---
+++ include/turtle_nav_cpp/nav_utils.hpp
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2022 Ⓒ Amro Al-Baali
+ * @file nav_utils.hpp
+ * @brief 2D navigation tools
+ * @author Amro Al-Baali (albaalia@live.com)
+ * @date 2022-Mar-21
+ */
+#ifndef TURTLE_NAV_CPP_NAV_UTILS_HPP_
+#define TURTLE_NAV_CPP_NAV_UTILS_HPP_
+
+#endif  // TURTLE_NAV_CPP_NAV_UTILS_HPP_
---
+++ src/nav_utils.cpp
@@ -0,0 +1,7 @@
+/**
+ * Copyright 2022 Ⓒ Amro Al-Baali
+ * @file nav_utils.cpp
+ * @brief Implementation of nav_utils.hpp
+ * @author Amro Al-Baali (albaalia@live.com)
+ * @date 2022-Mar-21
+ */
