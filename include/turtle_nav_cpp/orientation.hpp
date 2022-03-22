2e5350e12bd066eda946dc5a67a71bd45d409328
aalbaali <albaalia@live.com>
2022-03-22 04:02:12 +0000

    Initial implementation of the orientation class

---
+++ include/turtle_nav_cpp/orientation.hpp
@@ -0,0 +1,155 @@
+/**
+ * Copyright 2022 Ⓒ Amro Al-Baali
+ * @file orientation.hpp
+ * @brief Class for dealing with SO(2) headings
+ * @author Amro Al-Baali (albaalia@live.com)
+ * @date 2022-Mar-21
+ */
+#ifndef TURTLE_NAV_CPP_ORIENTATION_HPP_
+#define TURTLE_NAV_CPP_ORIENTATION_HPP_
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <geometry_msgs/msg/quaternion.hpp>
+#include <iostream>
+#include <turtlesim/msg/pose.hpp>
+
+namespace turtle_nav_cpp
+{
+namespace nav_utils
+{
+class Orientation
+{
+public:
+  /**
+   * @brief Default constructor
+   *
+   */
+  Orientation();
+
+  /**
+   * @brief Construct a new Orientation object
+   *
+   * @param[in] heading Orientation to insert
+   *
+   * @details The heading is automatically wrapped to (-pi, pi]
+   */
+  explicit Orientation(double heading);
+
+  /**
+   * @brief Construct a new Orientation object from an Eigen quaternion object
+   *
+   * @param[in] q Eigen quaternion
+   */
+  explicit Orientation(const Eigen::Quaterniond q);
+
+  /**
+   * @brief Construct a new Orientation object
+   *
+   * @param[in] q ROS `geometry_msgs` quaternion object
+   */
+  explicit Orientation(const geometry_msgs::msg::Quaternion q);
+
+  /**
+   * @brief Return the heading angle wrapped to (-pi, pi]
+   *
+   * @return double
+   */
+  double Angle() const;
+
+  /**
+   * @brief Return Eigen's rotation object
+   *
+   * @return Eigen::Rotation2Dd
+   */
+  Eigen::Rotation2Dd Rotation() const;
+
+  /**
+   * @brief Return rotation matrix
+   *
+   * @return Eigen::Matrix2d
+   */
+  Eigen::Matrix2d RotationMatrix() const;
+
+  /**
+   * @brief Wrap heading to pi and store rotation
+   *
+   * @param[in] heading Input heading
+   * @return Orientation&
+   */
+  Orientation & operator=(double heading);
+
+  /**
+   * @brief Assign heading
+   *
+   * @param[in] q Eigen quaternion object
+   * @return Orientation&
+   */
+  Orientation & operator=(const Eigen::Quaterniond q);
+
+  /**
+   * @brief Assign heading
+   *
+   * @param[in] q ROS `geometry_msgs` quaternion object
+   * @return Heading&
+   */
+  Orientation & operator=(const geometry_msgs::msg::Quaternion q);
+
+  /**
+   * @brief Multiply angle by a constant
+   *
+   * @details The angle is automatically wrapped to (-pi, pi]
+   *
+   * @param[in] other The heading in rhs
+   * @return Heading&
+   */
+  Orientation operator*(double other) const;
+
+  /**
+   * @brief Compoud headings
+   *
+   * @param[in] other The heading in rhs
+   * @return Orientation&
+   */
+  Orientation operator+(const Orientation & other) const;
+
+  /**
+   * @brief Compoud headings
+   *
+   * @param[in] other The heading in rhs
+   * @return Orientation&
+   */
+  Orientation operator+(double other) const;
+
+  /**
+   * @brief Subtract headings
+   *
+   * @param[in] other The heading in rhs
+   * @return Orientation&
+   */
+  Orientation operator-(const Orientation & other) const;
+
+  /**
+   * @brief Subtract headings
+   *
+   * @param[in] other The heading in rhs
+   * @return Orientation&
+   */
+  Orientation operator-(double other) const;
+
+  // TODO(aalbaali): Orientation(const turtle_nav_cpp::Pose)
+private:
+  Eigen::Rotation2Dd heading_;
+};
+
+/**
+ * @brief
+ *
+ * @param[in] os
+ * @param[in] heading
+ * @return std::ostream&
+ */
+std::ostream & operator<<(std::ostream & os, const Orientation & heading);
+}  // namespace nav_utils
+}  // namespace turtle_nav_cpp
+#endif  // TURTLE_NAV_CPP_ORIENTATION_HPP_
---
+++ src/orientation.cpp
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2022 Ⓒ Amro Al-Baali
+ * @file heading.cpp
+ * @brief Implementation of heading.hpp
+ * @author Amro Al-Baali (albaalia@live.com)
+ * @date 2022-Mar-21
+ */
+
+#include "turtle_nav_cpp/orientation.hpp"
+
+#include <Eigen/Dense>
+
+#include "turtle_nav_cpp/math_utils.hpp"
+#include "turtle_nav_cpp/nav_utils.hpp"
+
+namespace turtle_nav_cpp
+{
+namespace nav_utils
+{
+Orientation::Orientation() : heading_(0) {}
+
+Orientation::Orientation(double heading) : heading_(WrapToPi(heading)) {}
+
+Orientation::Orientation(const Eigen::Quaterniond q) : heading_(nav_utils::QuaternionToHeading(q))
+{
+}
+
+Orientation::Orientation(const geometry_msgs::msg::Quaternion q)
+: heading_(QuaternionMsgToHeading(q))
+{
+}
+
+double Orientation::Angle() const { return heading_.angle(); }
+
+Eigen::Rotation2Dd Orientation::Rotation() const { return heading_; }
+
+Eigen::Matrix2d Orientation::RotationMatrix() const { return heading_.toRotationMatrix(); }
+
+Orientation & Orientation::operator=(double heading)
+{
+  heading_ = Eigen::Rotation2Dd(WrapToPi(heading));
+  return *this;
+}
+
+Orientation & Orientation::operator=(const Eigen::Quaterniond q)
+{
+  heading_ = Eigen::Rotation2Dd(nav_utils::QuaternionToHeading(q));
+  return *this;
+}
+
+Orientation & Orientation::operator=(const geometry_msgs::msg::Quaternion q_msg)
+{
+  auto q = nav_utils::QuaternionMsgToQuaternion(q_msg);
+  q.normalize();
+  heading_ = Eigen::Rotation2Dd(nav_utils::QuaternionToHeading(q));
+  return *this;
+}
+
+Orientation Orientation::operator*(double other) const
+{
+  return Orientation(this->Angle() * other);
+}
+
+Orientation Orientation::operator+(const Orientation & other) const
+{
+  return Orientation(this->Angle() + other.Angle());
+}
+
+Orientation Orientation::operator+(double other) const
+{
+  return Orientation(this->Angle() + other);
+}
+
+Orientation Orientation::operator-(const Orientation & other) const
+{
+  return Orientation(this->Angle() - other.Angle());
+}
+
+Orientation Orientation::operator-(double other) const
+{
+  return Orientation(this->Angle() - other);
+}
+}  // namespace nav_utils
+}  // namespace turtle_nav_cpp
