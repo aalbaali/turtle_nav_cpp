85531878747343a2538437f5e2cad69a5a4001e7
aalbaali <albaalia@live.com>
2022-03-24 03:16:55 +0000

    Adding the rest of pose tests

--- test/test_pose.cpp
+++ test/test_pose.cpp
@@ -124,5 +124,92 @@ TEST_F(TestPose, Getters)
   EXPECT_DOUBLE_EQ(T.y(), y_);
   EXPECT_DOUBLE_EQ(T.angle(), theta_);
 }
+
+TEST_F(TestPose, Operators)
+{
+  // Default pose
+  Pose T;
+
+  // Assign ROS geometry pose
+  geometry_msgs::msg::Pose pose_msg;
+  pose_msg.position.x = x_;
+  pose_msg.position.y = y_;
+  pose_msg.orientation.x = 0;
+  pose_msg.orientation.y = 0;
+  pose_msg.orientation.z = sin(theta_ / 2);
+  pose_msg.orientation.w = cos(theta_ / 2);
+  T = pose_msg;
+  EXPECT_DOUBLE_EQ(T.x(), x_);
+  EXPECT_DOUBLE_EQ(T.y(), y_);
+  EXPECT_DOUBLE_EQ(T.angle(), theta_);
+
+  // Turtlesim pose
+  // Note that turtlesim stores data in floats
+  turtlesim::msg::Pose pose_turtle;
+  pose_turtle.x = x_;
+  pose_turtle.y = y_;
+  pose_turtle.theta = theta_;
+  T = Pose();  // Reset pose
+  T = pose_turtle;
+  EXPECT_FLOAT_EQ(T.x(), x_);
+  EXPECT_FLOAT_EQ(T.y(), y_);
+  EXPECT_FLOAT_EQ(T.angle(), theta_);
+
+  // Pose compounding
+  auto T1 = Pose(x_, y_, theta_);
+  Pose T2(1, 2, 0.1);
+  auto T3 = T1 * T2;
+  EXPECT_DOUBLE_EQ(T3.x(), x_ + 1);
+  EXPECT_DOUBLE_EQ(T3.y(), y_ + 2);
+  EXPECT_DOUBLE_EQ(T3.angle(), theta_ + 0.1);
+
+  // Transforming a vector into new frame
+  T = Pose(x_, y_, theta_);
+  Vector2d v{3, 4};
+  double x_expect = cos(theta_) * v(0) - sin(theta_) * v(1) + x_;
+  double y_expect = sin(theta_) * v(0) + cos(theta_) * v(1) + y_;
+  auto v2 = T * v;
+  EXPECT_DOUBLE_EQ(v2(0), x_expect);
+  EXPECT_DOUBLE_EQ(v2(1), y_expect);
+
+  // Self compounding
+  T *= Pose(5, 6, -0.1);
+  EXPECT_DOUBLE_EQ(T.x(), x_ + 5);
+  EXPECT_DOUBLE_EQ(T.y(), y_ + 6);
+  EXPECT_DOUBLE_EQ(T.angle(), theta_ - 0.1);
+}
+
+TEST_F(TestPose, Inverse)
+{
+  // T = [C r]
+  // T_inv = [C' -C'r]
+  const Pose T(x_, y_, theta_);
+  const auto T_inv = T.Inverse();
+  const double theta_expect = -theta_;
+  const double x_expect = -(cos(-theta_) * x_ - sin(-theta_) * y_);
+  const double y_expect = -(sin(-theta_) * x_ + cos(-theta_) * y_);
+  EXPECT_DOUBLE_EQ(T_inv.x(), x_expect);
+  EXPECT_DOUBLE_EQ(T_inv.y(), y_expect);
+  EXPECT_DOUBLE_EQ(T_inv.angle(), theta_expect);
+}
+
+TEST_F(TestPose, Ostream)
+{
+  const Pose T(x_, y_, theta_);
+  std::stringstream ss;
+  ss << T;
+  const std::string out_str = ss.str();
+
+  // Function that checks if a number is a substring of the output string
+  auto str_contains_double = [&](double val) {
+    std::stringstream ss;
+    ss << val;
+    EXPECT_TRUE(out_str.find(ss.str()) != std::string::npos);
+  };
+  // Check that each of the internal scalars is a substring of the output string
+  str_contains_double(x_);
+  str_contains_double(y_);
+  str_contains_double(theta_);
+}
 }  // namespace nav_utils
 }  // namespace turtle_nav_cpp
