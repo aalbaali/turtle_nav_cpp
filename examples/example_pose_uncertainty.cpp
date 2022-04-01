8ebbc89aa6cc43cd61cce5dce6a97d3c7c33a518
aalbaali <albaalia@live.com>
2022-04-01 04:28:50 +0000

    Generate a single trajectory and plot it

--- examples/example_pose_uncertainty.cpp
+++ examples/example_pose_uncertainty.cpp
@@ -11,36 +11,66 @@
 #include <Eigen/Dense>
 #include <algorithm>
 #include <cmath>
+#include <iostream>
+#include <random>
 #include <vector>
+
+#include "turtle_nav_cpp/math_utils.hpp"
+#include "turtle_nav_cpp/pose.hpp"
+
+namespace turtle_nav_cpp
+{
+void PlotPoses(const std::vector<nav_utils::Pose> & poses)
+{
+  // Get x and y values
+  std::vector<double> xvals;
+  std::vector<double> yvals;
+
+  std::for_each(poses.begin(), poses.end(), [&](const nav_utils::Pose & pose) {
+    xvals.push_back(pose.translation().x());
+    yvals.push_back(pose.translation().y());
+  });
+
+  matplot::plot(xvals, yvals);
+  matplot::grid(matplot::on);
+}
+}  // namespace turtle_nav_cpp
+
+using turtle_nav_cpp::nav_utils::Pose;
 
 int main()
 {
-  std::vector<double> x = matplot::linspace(0, 2 * M_PI);
-  std::vector<double> y = matplot::transform(x, [](auto x) { return sin(x); });
+  Pose T_1(0, 0, 0);
 
-  matplot::plot(x, y, "-o");
-  matplot::hold(matplot::on);
-  matplot::plot(x, matplot::transform(y, [](auto y) { return -y; }), "--xr")->line_width(2.5);
-  matplot::plot(x, matplot::transform(x, [](auto x) { return x / M_PI - 1.; }), "-:gs");
-  matplot::plot({1.0, 0.7, 0.4, 0.0, -0.4, -0.7, -1}, "k");
-  matplot::xlabel("x^2 vals");
-  matplot::ylabel("y vals");
-  matplot::grid(matplot::on);
-  matplot::gca()->minor_grid(true);
-  // matplot::show();
+  // Let the robot drive in a straight line with constant speed
+  const double dt = 0.01;     // sec
+  const double speed = 1;     // m/s
+  const double yaw_rate = 0;  // rad/s
 
-  // Attempting to plot ellipse
+  // The incremental change transformation matrix
+  const Pose dT(dt * speed, 0, dt * yaw_rate);
+
+  // Number of states
+  const int num_poses = 100;
+
+  std::vector<Pose> poses;
+  poses.reserve(num_poses);
+  poses.assign(100, Pose(0, 0, 0));
+  poses[0] = T_1;
+  for (int i = 1; i < num_poses; i++) {
+    poses[i] = poses[i - 1] * dT;
+  }
+
+  for (const auto & p : poses) {
+    std::cout << p << std::endl;
+  }
+
   matplot::figure();
-  // matplot::gca()->clear();
-  matplot::fimplicit([](double x, double y) { return pow(x, 2) + pow(y, 2) - 1; });
-  matplot::grid(matplot::on);
-  matplot::axis(matplot::equal);
-
-  matplot::figure();
-  matplot::scatter(x, y);
-  show();
-
-  // Eigen::Matrix2
+  turtle_nav_cpp::PlotPoses(poses);
+  matplot::xlabel("x [m]");
+  matplot::ylabel("y [m]");
+  matplot::title("Robot trajectory");
+  matplot::show();
 
   return 0;
 }
