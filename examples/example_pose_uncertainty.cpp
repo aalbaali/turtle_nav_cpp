748f2c75d8b6b4eeb01195148c54c57885572c15
aalbaali <albaalia@live.com>
2022-04-02 17:33:14 +0000

    Generate dead-reckoned trajectory

--- examples/example_pose_uncertainty.cpp
+++ examples/example_pose_uncertainty.cpp
@@ -11,6 +11,7 @@
 #include <Eigen/Dense>
 #include <algorithm>
 #include <cmath>
+#include <functional>
 #include <iostream>
 #include <random>
 #include <vector>
@@ -21,7 +22,7 @@
 namespace turtle_nav_cpp
 {
 /**
- * @brief Plot x-y positions of a vector of posee
+ * @brief Plot x-y positions of a vector of pose
  *
  * @param[in] poses Vector of poses to plot
  */
@@ -36,44 +37,60 @@ void PlotPoses(const std::vector<nav_utils::Pose> & poses)
     yvals.push_back(pose.translation().y());
   });
 
-  matplot::plot(xvals, yvals);
+  matplot::plot(xvals, yvals)->line_width(1.5);
   matplot::grid(matplot::on);
 }
 
 /**
- * @brief Generate a dead-reckoning trajectory
+ * @brief Generate a dead-reckoning trejectory from speed and yaw measurements
  *
- * @param[in] T_0 Initial/starting pose
- * @param[in] dT_vecs Vector of pose differences (i.e., T_1 = T_0 * dT_0)
- * @return std::vector<nav_utils::Pose>
+ * @param[in] T_0       Initial pose
+ * @param[in] dt        Sampling period [s]
+ * @param[in] speeds    Vector of speeds [m/s]
+ * @param[in] yaw_rates Vector of yaw rates [rad/s]
+ * @return std::vector<nav_utils::Pose> Vector of poses (i.e., the trajectory)
  */
-std::vector<nav_utils::Pose> GenerateTrajectory(
-  const nav_utils::Pose & T_0, const std::vector<nav_utils::Pose> & dT_vecs)
+std::vector<nav_utils::Pose> DeadReckonTrajectory(
+  const nav_utils::Pose & T_0, const double dt, const std::vector<double> & speeds,
+  const std::vector<double> & yaw_rates)
 {
+  const size_t num_poses = speeds.size() + 1;
   std::vector<nav_utils::Pose> poses;
-  poses.reserve(dT_vecs.size() + 1);
-  poses.assign(100, nav_utils::Pose(0, 0, 0));
-  poses[0] = T_0;
-  for (size_t i = 1; i < dT_vecs.size(); i++) {
-    poses[i] = poses[i - 1] * dT_vecs[i - 1];
+  poses.reserve(num_poses);
+  poses.push_back(T_0);
+
+  std::vector<nav_utils::Pose> dT_vecs(num_poses);
+  for (size_t i = 1; i < speeds.size(); i++) {
+    nav_utils::Pose dT_km1(dt * speeds[i - 1], 0, dt * yaw_rates[i - 1]);
+    auto T_k = poses[i - 1] * dT_km1;
+    poses.push_back(T_k);
   }
 
+  // Generate trajectory
   return poses;
 }
 
-std::vector<nav_utils::Pose> GenerateStraightLine(
-  const nav_utils::Pose & T_0, const size_t num_poses, const double dt, const double speed,
-  const double yaw_rate, const bool generate_noise = false)
+/**
+ * @brief Generate a vector<double> of a samples of a Gaussian RV for a given mean and standard
+ *        deviation
+ *
+ * @param[in] num_vals          Number of values/samples to generate
+ * @param[in] mean              Mean of the Gaussian random variable
+ * @param[in] stddev            Standard deviation of the Gaussian random variable
+ * @param[in] rn_generator      Random number generator
+ * @return std::vector<double>  Vector of realizations
+ */
+std::vector<double> GenerateNoisyVector(
+  const size_t num_vals, const double mean, const double stddev,
+  std::default_random_engine & rn_generator)
 {
-  // The incremental change transformation matrix
-  const nav_utils::Pose dT(dt * speed, 0, dt * yaw_rate);
-  std::vector<nav_utils::Pose> dT_vecs;
-  dT_vecs.assign(num_poses - 1, dT);
+  auto randn = [&rn_generator]() { return turtle_nav_cpp::randn_gen(rn_generator); };
 
-  // Generate trajectory
-  return turtle_nav_cpp::GenerateTrajectory(T_0, dT_vecs);
+  std::vector<double> noisy_vals(num_vals);
+  std::generate(noisy_vals.begin(), noisy_vals.end(), [&]() { return mean + stddev * randn(); });
+
+  return noisy_vals;
 }
-
 }  // namespace turtle_nav_cpp
 
 using turtle_nav_cpp::nav_utils::Pose;
@@ -86,12 +103,18 @@ int main()
   const int num_poses = 100;
 
   // Let the robot drive in a straight line with constant speed
-  const double dt = 0.01;     // sec
+  const double dt = 0.1;      // sec
   const double speed = 1;     // m/s
   const double yaw_rate = 0;  // rad/s
 
+  std::default_random_engine rn_generator = std::default_random_engine();
+
+  // Noisy speeds
+  auto speeds = turtle_nav_cpp::GenerateNoisyVector(num_poses - 1, speed, 0.01, rn_generator);
+  auto yaw_rates = turtle_nav_cpp::GenerateNoisyVector(num_poses - 1, yaw_rate, 1, rn_generator);
+
   // Generate straight-line trajectory
-  auto poses = turtle_nav_cpp::GenerateStraightLine(T_0, num_poses, dt, speed, yaw_rate, false);
+  auto poses = turtle_nav_cpp::DeadReckonTrajectory(T_0, dt, speeds, yaw_rates);
 
   for (const auto & p : poses) {
     std::cout << p << std::endl;
@@ -102,6 +125,7 @@ int main()
   matplot::xlabel("x [m]");
   matplot::ylabel("y [m]");
   matplot::title("Robot trajectory");
+  matplot::grid(true);
   matplot::show();
 
   return 0;
